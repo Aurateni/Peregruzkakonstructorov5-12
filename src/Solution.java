9aabbfb7931a59f7be048c5123a8e851203a4cf6
Arsenii <aurateni@gmail.com>
2020-10-20 10:18:24 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+
+    public Solution(String f) {
+    }
+    private Solution(int i) {
+    }
+     Solution(String f, int i) {
+    }
+    protected Solution(String f, int i, int e) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/SubSolution.java
@@ -0,0 +1,13 @@
+public class SubSolution extends Solution {
+    public SubSolution(String f) {
+        super(f);
+    }
+
+    SubSolution(String f, int i) {
+        super(f, i);
+    }
+
+    protected SubSolution(String f, int i, int e) {
+        super(f, i, e);
+    }
+}
